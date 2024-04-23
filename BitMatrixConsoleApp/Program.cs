d3fc729cb70b0d9e7d94f9a643236d39cef0632a
minislel <marcin4kt@gmail.com>
2024-04-24 00:44:53 +0200

    added tostring, started a constructor

---
+++ BitMatrixConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BitMatrixConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var m = new BitMatrix(2, 2, new int[] { 1, 0, 0, 1 });
+            Console.WriteLine(m);
+
+            m = new BitMatrix(2, 2, 1, 0, 0, 0);
+            Console.WriteLine(m);
+        }
+    }
+}
--- BitMatrixLib/Class1.cs
+++ BitMatrixLib/Class1.cs
@@ -1,7 +1,77 @@
-﻿namespace BitMatrixLib
-{
-    public class Class1
-    {
+﻿using System;
+using System.Collections;
+using System.Text;
 
+// prostokątna macierz bitów o wymiarach m x n
+public partial class BitMatrix
+{
+    private BitArray data;
+    public int NumberOfRows { get; }
+    public int NumberOfColumns { get; }
+    public bool IsReadOnly => false;
+
+    // tworzy prostokątną macierz bitową wypełnioną `defaultValue`
+    public BitMatrix(int numberOfRows, int numberOfColumns, int defaultValue = 0)
+    {
+        if (numberOfRows < 1 || numberOfColumns < 1)
+            throw new ArgumentOutOfRangeException("Incorrect size of matrix");
+        data = new BitArray(numberOfRows * numberOfColumns, BitToBool(defaultValue));
+        NumberOfRows = numberOfRows;
+        NumberOfColumns = numberOfColumns;
+    }
+    public BitMatrix(int numberOfRows, int numberOfColumns, params int[] bits)
+    {
+        if (numberOfRows < 1 || numberOfColumns < 1)
+            throw new ArgumentOutOfRangeException("Incorrect size of matrix");
+        if (bits == null || bits.Length == 0)
+        {
+            data = new BitArray(numberOfRows * numberOfColumns, false);
+        }
+        else 
+        {
+            data = new BitArray(numberOfRows * numberOfColumns, false);
+            for (int i = 0; i < numberOfRows; i++) 
+            {
+                for (int j = 0; j < numberOfColumns; j++)
+                {
+                    if (j < numberOfColumns)
+                    {
+                        if (bits[i+j] != 0)
+                        {
+                            data[i + j] = true;
+                        }
+                        else
+                        {
+                            data[i + j] = false;
+                        }
+                    }
+                }
+            }
+        }
+        NumberOfRows = numberOfRows;
+        NumberOfColumns = numberOfColumns;
+    }
+
+    public static int BoolToBit(bool boolValue) => boolValue ? 1 : 0;
+    public static bool BitToBool(int bit) => bit != 0;
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sbrow = new StringBuilder();
+        for (int i = 0; i < NumberOfRows; i++)
+        {
+            for (int j = 0; j < NumberOfColumns; j++)
+            {
+                if (j < NumberOfColumns)
+                {
+                    sbrow.Append(BoolToBit(data[j + i]));
+                }
+            }
+            sb.Append(sbrow);
+            sb.Append(Environment.NewLine);
+            sbrow.Clear();
+        }
+        return sb.ToString();
     }
 }
